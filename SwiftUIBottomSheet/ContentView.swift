957fa27f16f63e56bef0e13975eb8528ecddc1ff
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2022-12-14 17:04:40 +0630

    Initial Commit.

---
+++ SwiftUIBottomSheet/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  SwiftUIBottomSheet
+//
+//  Created by Simon Ng on 22/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(restaurants) { restaurant in
+                    BasicImageRow(restaurant: restaurant)
+                }
+            }
+            .listStyle(.plain)
+            
+            .navigationTitle("Restaurants")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct BasicImageRow: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        HStack {
+            Image(restaurant.image)
+                .resizable()
+                .frame(width: 40, height: 40)
+                .cornerRadius(5)
+            Text(restaurant.name)
+        }
+    }
+}
+
---
+++ SwiftUIBottomSheet/SwiftUIBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBottomSheetApp.swift
+//  SwiftUIBottomSheet
+//
+//  Created by Simon Ng on 22/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
